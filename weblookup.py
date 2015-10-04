082885eafc81dc0d9a8d28880659f036451a71c9
Amit Raut <raut.am@husky.neu.edu>
2015-10-04 14:28:46 -0400

    Updated utility

--- weblookup.py
+++ weblookup.py
@@ -1,50 +1,23 @@
 #!/usr/bin/env python
-import socket, xlwt, dns.resolver, os
+
+import subprocess as sp, time
 
 
-def getMXRecord(url_record):
-	result = dns.resolver.query(url_record, 'mx')
-	output = []
-	for d in result:
-		output.append(str(d.exchange))
-	return output
+def getHostnameAndIPaddr(url_addr):
+	command = 'nslookup ' + url_addr + ' | grep ' + url_addr[:-1]
+	with open('/Users/AR/Desktop/output.txt', 'a') as f: #This is the path for the output file
+		ps = sp.Popen(command, stderr=sp.PIPE, stdout=f, shell=True)
+		ps.communicate()[0]
 
-with open('/Users/AR/Desktop/url_list.txt', 'r') as f:
+		# ps.terminate()
 
-	book = xlwt.Workbook(encoding="utf-8")
-	sheet1 = book.add_sheet("Sheet 1")
+def main():
+	with open('/Users/AR/Desktop/url_list.txt', 'r') as f: #Use the correct absolute path for the url_list file
+		for line in f:
+			getHostnameAndIPaddr(line)
 
-	sheet1.write(0,0, 'Hostname')
-	sheet1.write(0,1, 'IP Address')
-	sheet1.write(0,2, 'MX Record')
-	sheet1.write(0,3, 'MX IP Address')
-	hostIPList = []
-	url = ''
+if __name__ == '__main__':
+	main()
 
-	for line in f:
-		if line[-1] == '\n':
-			url = line[:-1]
-		else:
-			url = line
-		hostIPList.append((url, socket.gethostbyname(url), \
-		getMXRecord(url)))
 
-	print '{0:13s}     {1:18s}     {2:30s}     {3:18s}    {4:8.8}\n'\
-	.format('Hostname', 'IP Address', 'MX Record', 'MX IP Address', 'Status')
-	rowcount = 1
-	for record in hostIPList:
-		for i in xrange(len(record[2])):
-			print '{0:13s}     {1:18.15s}     {2:30s}     {3:18s}    {4:8.8}'\
-			.format(record[0], record[1], record[2][i], socket.gethostbyname(record[2][i]), 
-				'Up!' if os.system("ping -c 2 " + record[2][i] + ' | 2>&1') == 0 else 'Down!')
 
-			sheet1.write(rowcount, 0, record[0])
-			sheet1.write(rowcount, 1, record[1])
-			sheet1.write(rowcount, 2, record[2][i])
-			sheet1.write(rowcount, 3, socket.gethostbyname(record[2][i]))
-			sheet1.write(rowcount, 4, 'Up!' if os.system("ping -c 2 " + record[2][i] + ' | 2>&1') == 0 else 'Down!')
-			rowcount += 1
-
-	book.save('/Users/AR/Desktop/IPlookups.xls')
-	print '\nSpreadsheet saved!'
-		
